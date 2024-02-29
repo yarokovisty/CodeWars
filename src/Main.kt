1cecddf00b634ef4d68b89a49a0b147e841a2736
YarokovistY <yarokovisty@gmail.com>
2024-02-29 12:34:12 +0700

    Add exercises 8 kyu

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,14 @@
 import kyu6.beggars
 import kyu6.blackOrWhiteKey
 import kyu6.findMissingLetter
+import kyu7.toIndustrial
+import kyu7.toNormal
 import kyu8.*
 import java.lang.Exception
 
 fun main() {
-    println(MixedSum().sum(listOf(9, 3, "7", "3")))
+    println(toNormal(0.33))
+
 
 }
+
